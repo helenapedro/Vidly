407be3447856660e1171f5568d9c724391810afb
Helena Pedro <mbeua94@gmail.com>
2024-03-05 13:23:48 +0100

    new commit

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
+const Joi = require('joi'); 
 const express = require('express');
 const app = express()
 
+app.use(express.json());
+
+const genres = [
+     { id: 1, name: 'Anime' },  
+     { id: 2, name: 'Drama Movies' },  
+     { id: 3, name: 'Independent Movies' },
+];
 
 app.get('/api/genres', (req, res) => {
      res.send(genres);
 });
 
-app.get('/api/genres/:id', (req, res) => {
-     genres.find(c => c.id == req.params.id);
+app.post('/api/genres', (req, res) => {
+     // input validation
+     const { error } = validateGenre(req.body);
+     if (error) return res.status(400).send(error.details[0].message);
+
+     const genre = {
+          id: genres.length + 1,
+          name: req.body.name
+     };
+     genres.push(genre);
+     res.send(genre);
 });
 
-/* app.post()
-app.put()
-app.delete() */
+// We use the put method for updating resources
+app.put('/api/genres/:id', (req, res) => {
+     const genre = genres.find(c => c.id === parseInt(req.params.id));
+     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+   
+     const { error } = validateGenre(req.body); 
+     if (error) return res.status(400).send(error.details[0].message);
+     
+     genre.name = req.body.name; 
+     res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+     const genre = genres.find(c => c.id === parseInt(req.params.id));
+     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+   
+     const index = genres.indexOf(genre);
+     genres.splice(index, 1);
+   
+     res.send(genre);
+});
 
 
+app.get('/api/genres/:id', (req, res) => {
+     const genre = genres.find(c => c.id === parseInt(req.params.id));
+     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+     res.send(genre);
+});
+
+function validateGenre(genre) {
+     const schema = Joi.object({
+         name: Joi.string().min(3).required()
+     });
+     return schema.validate(genre);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
