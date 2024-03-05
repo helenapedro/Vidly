2d03ee11810edbd78798161ca3926d0b49d03a43
Helena Pedro <mbeua94@gmail.com>
2024-03-05 18:40:35 +0100

    new commit

--- routes/customers.js
+++ routes/customers.js
@@ -1,26 +1,7 @@
-const Joi = require('joi');
+const {Customer, validate} = require('../models/customer'); 
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  isGold: {
-     type: Boolean,
-     default: false
-  },
-  phone: {
-     type: String,
-     required: true,
-     minlength: 5,
-     maxlength: 50
-   }
-}));
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -28,13 +9,13 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validateCustomer(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({ 
-     name: req.body.name,
-     phone: req.body.phone,
-     isGold: req.body.isGold   
+    name: req.body.name,
+    isGold: req.body.isGold,
+    phone: req.body.phone
   });
   customer = await customer.save();
   
@@ -42,12 +23,15 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateCustomer(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-    new: true
-  });
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    { 
+      name: req.body.name,
+      isGold: req.body.isGold,
+      phone: req.body.phone
+    }, { new: true });
 
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
   
@@ -70,13 +54,4 @@ router.get('/:id', async (req, res) => {
   res.send(customer);
 });
 
-function validateCustomer(customer) {
-     const schema = Joi.object({
-         name: Joi.string().min(5).max(50).required(),
-         phone: Joi.string().min(5).max(50).required(),
-         isGold: Joi.boolean
-     });
-     return schema.validate(customer, schema);
-}
-
-module.exports = router;
+module.exports = router; 
