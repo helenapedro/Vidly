dcbc0f3ad45e8837366422e2c0fab7a34fbcab2d
Helena Pedro <mbeua94@gmail.com>
2024-03-06 20:27:47 +0100

    new commit

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
-//Joi.objectId = require('joi-objectid')(Joi);
-require('express-async-errors');
 const winston = require('winston');
-require('winston-mongodb');
-const error = require('./middleware/error');
-const config = require('config');
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const express = require('express');
-const app = express();
-
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.MongoDB({ 
-  db: 'mongodb://127.0.0.1:27017/vidly' ,
-  level: 'info'
-}));
-
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('Fatal Error: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
-
-mongoose.connect('mongodb://127.0.0.1:27017/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-
-
-/* const winston = require('winston');
 const express = require('express');
 const app = express();
 
@@ -60,7 +11,7 @@ require('./startup/validation')();
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
-module.exports = server; */
+module.exports = server;
 
 
 
