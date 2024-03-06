58be34bf6984512914af037b4f4d1c85b39f6525
Helena Pedro <mbeua94@gmail.com>
2024-03-06 18:03:18 +0100

    new commit

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,14 @@
-/* const auth = require('../middleware/auth');
-*/
-const config = require('config');
+const auth = require('../middleware/auth'); // this auth represents authorization not authentication 
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-/* router.get('/me', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
   const user = await User.findById(req.user._id).select('-password');
   res.send(user);
-}); */
+});
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
