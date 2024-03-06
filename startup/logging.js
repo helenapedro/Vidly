0a2550d8c510e1e39a495d7522a3286e647220f3
Helena Pedro <mbeua94@gmail.com>
2024-03-06 20:59:26 +0100

    new commit

--- startup/logging.js
+++ startup/logging.js
@@ -1,9 +1,9 @@
 const winston = require('winston');
-// require('winston-mongodb');
+require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.handleExceptions(
+  winston.exceptions.handle(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
   
@@ -11,9 +11,9 @@ module.exports = function() {
     throw ex;
   });
   
-  winston.add(winston.transports.File, { filename: 'logfile.log' });
-  // winston.add(winston.transports.MongoDB, { 
-  //   db: 'mongodb://127.0.0.1:27017/vidly',
-  //   level: 'info'
-  // });  
+  winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+  winston.add(new winston.transports.MongoDB, { 
+    db: 'mongodb://127.0.0.1:27017/vidly',
+    level: 'info'
+  });  
 }
