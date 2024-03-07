8d0d6e04bc8fd945491ddd87d0113e72d562a2ff
Helena Pedro <mbeua94@gmail.com>
2024-03-07 16:45:29 +0100

    new commit

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -1,11 +1,11 @@
-const {User} = require('../../../models/user');
+const { User } = require('../../../models/user');
 const auth = require('../../../middleware/auth');
 const mongoose = require('mongoose');
 
 describe('auth middleware', () => {
   it('should populate req.user with the payload of a valid JWT', () => {
     const user = { 
-      _id: mongoose.Types.ObjectId().toHexString(), 
+      _id: new mongoose.Types.ObjectId().toHexString(), 
       isAdmin: true 
     };
     const token = new User(user).generateAuthToken();
