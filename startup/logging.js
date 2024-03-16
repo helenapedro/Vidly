30372856a6213134f2709eeeff24a4bba32cd124
Helena Pedro <mbeua94@gmail.com>
2024-03-16 17:33:45 +0100

    new commit

--- startup/logging.js
+++ startup/logging.js
@@ -13,7 +13,7 @@ module.exports = function() {
   
   winston.add(new winston.transports.File({ filename: 'logfile.log' }));
   winston.add(new winston.transports.MongoDB({ 
-    db: 'mongodb://127.0.0.1:27017/vidly',
+    db: 'mongodb+srv://username:<password>@cluster0.iqa6m5k.mongodb.net/',
     level: 'info',
     options: { useUnifiedTopology: true }
   }));
